317196d4f5522d80427890c1da08162b930924dc
Pingou <pingou@users.noreply.github.com>
2016-09-04 21:53:39 +0200

    test

--- app/src/main/java/github/us/github/MainActivity.java
+++ app/src/main/java/github/us/github/MainActivity.java
@@ -7,6 +7,7 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        //it's a nice Activity
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
